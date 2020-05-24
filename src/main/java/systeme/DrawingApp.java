bd22b59cdda25e871053e75cb11ef15c486015f5
payam394 <payam.ak394@gmail.com>
2020-05-24 23:43:02 +0200

    userinput Done. => All Done.

--- src/main/java/systeme/DrawingApp.java
+++ src/main/java/systeme/DrawingApp.java
@@ -1,5 +1,6 @@
 package systeme;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DrawingApp {
 	
@@ -10,20 +11,37 @@ public class DrawingApp {
 		
 		DrawingTUI tui = new DrawingTUI(AllllllDesigns);
 		
-		Point p= new Point(4, 4);
-		Dessin s1 = new Cercle("c1", "cercle", p, 10);
-		AllllllDesigns.add(s1);
+		Scanner myObj = new Scanner(System.in);
 		
-		tui.nextCommand("deplacer");
-		AllllllDesigns.get(0).afficher();
-		
-		
-		tui.nextCommand("Carre");
-		tui.nextCommand("Triangle");
-		
-		for (Dessin i : AllllllDesigns) {
-			i.afficher();
+//		
+//		Point p= new Point(4, 4);
+//		Dessin s1 = new Cercle("c1", "cercle", p, 10);
+//		AllllllDesigns.add(s1);
+//		
+//		tui.nextCommand("deplacer");
+//		AllllllDesigns.get(0).afficher();
+//		
+		/*
+		tui.nextCommand("cer1 = Cercle((0, 0), 50)");
+		tui.nextCommand("car1 = Carre((0, 0), 30)");
+		tui.nextCommand("tr1 = Triangle((0, 0),(3, 5), (7, -1))");
+		tui.nextCommand("rec1 = Rectengle((1, 2), (6, 3))");
+		//tui.nextCommand("Triangle");
+		*/
+		while(true) {
+			String userInput = myObj.nextLine();
+			tui.nextCommand(userInput);
+			
+			/*
+			for (Dessin i : AllllllDesigns) {
+				i.afficher();
+			}
+			*/
 		}
+		
+		
+		
+		
 		
 	}
 
--- src/main/java/systeme/DrawingTUI.java
+++ src/main/java/systeme/DrawingTUI.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import actions.ActionExecuteur;
 import actions.Command;
 import actions.actionCreerCarre;
+import actions.actionCreerCercle;
+import actions.actionCreerRectengle;
 import actions.actionCreerTriangle;
 import actions.actionDeplacer;
 
@@ -17,29 +19,62 @@ public class DrawingTUI implements Command{
 		super();
 		AllDesigns = allDesigns;
 	}
+	
+	public int toInt(String s) {
+		int i = Integer.parseInt(s);
+		return i;
+	}
 
 
 	public Command nextCommand(String text) {	//implements Command
 		
-		
 		ActionExecuteur Exe = new ActionExecuteur();
 		
-		switch (text){
+		String[] words= text.split("\\W+");
+		//String command= words[0];
+		
+		switch(words[1]) {
+		
+		case "Carre" :
+			System.out.println("Creer nouvelle carre");
+			
+			Exe.executeCommand(new actionCreerCarre(words[0], toInt(words[2]), toInt(words[3]), toInt(words[4]), AllDesigns));
+			break;
+			
+		case "Cercle" :
+			System.out.println("Creer nouvelle cercle");
+			
+			Exe.executeCommand(new actionCreerCercle(words[0], toInt(words[2]), toInt(words[3]), toInt(words[4]), AllDesigns));
+			break;
+			
+		case "Triangle" :
+			System.out.println("Creer nouvelle triangle");
+			Exe.executeCommand(new actionCreerTriangle(words[0], toInt(words[2]), toInt(words[3]), toInt(words[4]), toInt(words[5]), toInt(words[6]), toInt(words[7]), AllDesigns));
+			break;
+			
+		case "Rectengle" :
+			System.out.println("Creer nouvelle rectengle");
+			Exe.executeCommand(new actionCreerRectengle(words[0], toInt(words[2]), toInt(words[3]), toInt(words[4]), toInt(words[5]), AllDesigns));
+			break;
+		}
+		
+		
+		
+		
+		switch (words[0]){
 		
 		case "deplacer" :
 			System.out.println("Executing Command Deplacer");
 			Exe.executeCommand(new actionDeplacer( AllDesigns.get(0), 1, 3));
 			break;
 			
-		case "Carre" :
-			System.out.println("Creer nouvelle carre");
-			Exe.executeCommand(new actionCreerCarre("Cr1", 3, 5, 20, AllDesigns));
+		
+		case "move" :
+			System.out.println("Executing Command Deplacer");
+			Exe.executeCommand(new actionDeplacer( AllDesigns.get(0), 1, 3));
 			break;
 			
-		case "Triangle" :
-			System.out.println("Creer nouvelle triangle");
-			Exe.executeCommand(new actionCreerTriangle("T1", 3, 5, 2, 6, 9, 10, AllDesigns));
-			break;
+		
 		
 			
 		
@@ -52,8 +87,14 @@ public class DrawingTUI implements Command{
 		return null;
 		
 	}
+	/*
+	public void getIndexOfDessin(String nom) {
+		for (Dessin i : AllDesigns) {
+			if (i.)
+		}
+	}
 	
-	
+	*/
 	public void afficheDessin() {
 		
 	}
