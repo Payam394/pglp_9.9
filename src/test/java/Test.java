0608d4774b3c63efc8543d58c724a7182ced4344
payam394 <payam.ak394@gmail.com>
2020-05-24 23:55:14 +0200

    Test Deplacer Regrouper Done

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -12,7 +12,7 @@ public class Test {
 	DrawingTUI tui = new DrawingTUI(AllllllDesigns);
 	
 	@org.junit.Test
-	public void test() {
+	public void testCreeation() {
 		tui.nextCommand("cer1 = Cercle((0, 0), 50)");
 		tui.nextCommand("car1 = Carre((0, 0), 30)");
 		tui.nextCommand("tr1 = Triangle((0, 0),(3, 5), (7, -1))");
@@ -23,6 +23,17 @@ public class Test {
 			i.afficher();
 		}
 		
+		
+		
+		tui.nextCommand("deplacer( cer1, (9, 10)");
+		AllllllDesigns.get(0).afficher();
+		
+		
+		tui.nextCommand("move( rec1, (20, 30))");
+		AllllllDesigns.get(3).afficher();
+		
 	}
+	
+	
 
 }
